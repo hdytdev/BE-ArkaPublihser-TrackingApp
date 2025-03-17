c0c0e0f08f0c4262cc885601ea11383c2a562015
Dadan Hidayat <dadanhidyt@gmail.com>
2025-03-17 20:58:19 +0700

    setting

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,15 @@
 
 use App\Http\Controllers\Admin\CustomerController;
 use Illuminate\Support\Facades\Route;
+use Livewire\Livewire;
 
 
+Livewire::setUpdateRoute(function($handle){
+    return Route::post('/server/wire',$handle);
+});
+Livewire::setScriptRoute(function($handle){
+    return Route::get('/application.js',$handle);
+});
 Route::prefix("admin")->name('admin.')->group(function () {
     Route::get('/', \App\Http\Controllers\Admin\HomeController::class)->name('home');
 
